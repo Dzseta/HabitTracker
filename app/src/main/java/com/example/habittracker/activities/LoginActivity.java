b854db666477abbbfd9b6484345b3be42b7a4ce0
Barát Aszparuh Zoltán <barat.aszparuh@gmail.com>
2024-01-29 01:30:30 +0100

    Added FireBase. Added register and login. Connected authentication to FireBase.

--- app/src/main/java/com/example/habittracker/activities/LoginActivity.java
+++ app/src/main/java/com/example/habittracker/activities/LoginActivity.java
@@ -1,19 +1,73 @@
 package com.example.habittracker.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.habittracker.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    // EditTexts
+    EditText editTextTextEmailAddress;
+    EditText editTextTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // EditTexts
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent i = new Intent();
+            i.setClass(this, TodayActivity.class);
+            startActivity(i);
+        }
+    }
+
+    // login
+    public void login(View view) {
+        mAuth.signInWithEmailAndPassword(editTextTextEmailAddress.getText().toString(), editTextTextPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // sign in success
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // go to app
+                            Intent i = new Intent();
+                            i.setClass(getApplicationContext(), TodayActivity.class);
+                            startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     // onClick - open new activity
@@ -22,10 +76,6 @@ public class LoginActivity extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.registerButton:
                 i.setClass(this, RegisterActivity.class);
-                startActivity(i);
-                break;
-            case R.id.loginButton:
-                i.setClass(this, TodayActivity.class);
                 startActivity(i);
                 break;
             default: break;
--- app/src/main/java/com/example/habittracker/activities/RegisterActivity.java
+++ app/src/main/java/com/example/habittracker/activities/RegisterActivity.java
@@ -1,30 +1,61 @@
 package com.example.habittracker.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.habittracker.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    // EditTexts
+    EditText editTextTextEmailAddress;
+    EditText editTextTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // EditTexts
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
     }
 
-    // onClick - open new activity
-    public void openActivity(View view) {
-        Intent i = new Intent();
-        switch (view.getId()) {
-            case R.id.registerButton:
-                i.setClass(this, TodayActivity.class);
-                startActivity(i);
-                break;
-            default: break;
-        }
+    // register
+    public void register(View view) {
+        mAuth.createUserWithEmailAndPassword(editTextTextEmailAddress.getText().toString(), editTextTextPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // sign in success
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // go to app
+                            Intent i = new Intent();
+                            i.setClass(getApplicationContext(), TodayActivity.class);
+                            startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
 }
