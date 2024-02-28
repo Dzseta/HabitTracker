7aed86ff405f0f11f802a8eee0c98f0c50fb6405
Barát Aszparuh Zoltán <barat.aszparuh@gmail.com>
2024-02-28 18:09:58 +0100

    Category model added

---
+++ app/src/main/java/com/example/habittracker/models/Category.java
@@ -0,0 +1,39 @@
+package com.example.habittracker.models;
+
+public class Category {
+
+    // variables
+    private String icon;
+    private String name;
+    private String color;
+
+    public Category(String icon, String name, String color) {
+        this.icon = icon;
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
