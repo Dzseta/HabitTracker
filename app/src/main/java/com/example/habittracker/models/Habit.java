d90cfed402a0a02ca4e06e1da3a0685960052934
Barát Aszparuh Zoltán <barat.aszparuh@gmail.com>
2024-02-28 18:27:55 +0100

    Habit model v0.1 added

---
+++ app/src/main/java/com/example/habittracker/models/Habit.java
@@ -0,0 +1,112 @@
+package com.example.habittracker.models;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Habit {
+
+    // variables
+    private Category category;
+    private String type;    // yesNo, number, timer, checklist
+    private String name;
+    private String description;
+    private String repeatType;
+    private String repeatNumber;
+    private Date startDate;
+    private Date endDate;
+    private int priority;
+    private Time reminder;
+
+    public Habit(Category category, String type, String name, String description, String repeatType, String repeatNumber, Date startDate, Date endDate, int priority, Time reminder) {
+        this.category = category;
+        this.type = type;
+        this.name = name;
+        this.description = description;
+        this.repeatType = repeatType;
+        this.repeatNumber = repeatNumber;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.priority = priority;
+        this.reminder = reminder;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRepeatType() {
+        return repeatType;
+    }
+
+    public void setRepeatType(String repeatType) {
+        this.repeatType = repeatType;
+    }
+
+    public String getRepeatNumber() {
+        return repeatNumber;
+    }
+
+    public void setRepeatNumber(String repeatNumber) {
+        this.repeatNumber = repeatNumber;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Time getReminder() {
+        return reminder;
+    }
+
+    public void setReminder(Time reminder) {
+        this.reminder = reminder;
+    }
+}
